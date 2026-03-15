d7089c1db67638df1ed66b4277119e07b5239636
Trigo <trigovandevosse@gmail.com>
2026-03-15 12:27:04 +0100

    auth login pagina
    login pagina met breeze

--- wap-app/resources/views/layouts/auth/simple.blade.php
+++ wap-app/resources/views/layouts/auth/simple.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased">
+    <body class="min-h-screen bg-white antialiased text-black">
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
